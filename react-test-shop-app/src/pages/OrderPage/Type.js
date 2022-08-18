d955d0f35cc7fc4ad7752e7b0e16cb3436bc5a29
Hyunsoo Lee <wrkbrs@gmail.com>
2022-08-18 19:51:25 +0900

    add error state

--- react-test-shop-app/src/pages/OrderPage/Type.js
+++ react-test-shop-app/src/pages/OrderPage/Type.js
@@ -1,16 +1,18 @@
 import axios from "axios";
 import React, { useCallback, useEffect, useState } from "react";
+import ErrorBanner from "../../components/ErrorBanner";
 import Products from "./Products";
 
 function Type({ orderType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   const loadItems = useCallback(async (orderType) => {
     try {
       const res = await axios.get(`http://localhost:5000/${orderType}`);
       setItems(res.data);
     } catch (error) {
-      console.error(error);
+      setError(true);
     }
   }, []);
 
@@ -27,6 +29,10 @@ function Type({ orderType }) {
   useEffect(() => {
     loadItems(orderType);
   }, []);
+
+  if (error) {
+    return <ErrorBanner message="에러가 발생했습니다." />;
+  }
 
   return <div>{optionItems}</div>;
 }
