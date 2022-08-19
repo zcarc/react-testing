7bc753f3e342b400e8aba5c088a7641ca908cd32
Hyunsoo Lee <wrkbrs@gmail.com>
2022-08-19 22:57:26 +0900

    add Options test

--- react-test-shop-app/src/pages/OrderPage/tests/Type.test.js
+++ react-test-shop-app/src/pages/OrderPage/tests/Type.test.js
@@ -30,3 +30,10 @@ test("product 데이터들을 가져올 때, 에러를 만납니다.", async () 
   const errorBanner = await screen.findByTestId("error-banner");
   expect(errorBanner).toHaveTextContent("에러가 발생했습니다.");
 });
+
+test("서버에서 option 정보 가져오기", async () => {
+  render(<Type orderType="options" />);
+
+  const optionCheckboxes = await screen.findAllByRole("checkbox");
+  expect(optionCheckboxes).toHaveLength(2);
+});
