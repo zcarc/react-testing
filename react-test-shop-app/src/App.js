0c7157d53ba3b072b4727a1a511600d7f6af9cf3
Hyunsoo Lee <wrkbrs@gmail.com>
2022-08-19 23:58:51 +0900

    fix App

--- react-test-shop-app/src/App.js
+++ react-test-shop-app/src/App.js
@@ -1,13 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-import SummaryPage from "./pages/SummaryPage/SummaryPage";
+import OrderPage from "./pages/OrderPage/OrderPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <SummaryPage />
-      </header>
+    <div style={{ padding: "4rem" }}>
+      <OrderPage />
     </div>
   );
 }
