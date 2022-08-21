34a9c4e1530ab2ecec305942547d2de8be66901a
Hyunsoo Lee <wrkbrs@gmail.com>
2022-08-22 01:02:15 +0900

    add OrderContext

---
+++ react-test-shop-app/src/contexts/OrderContext.js
@@ -0,0 +1,58 @@
+import { createContext, useMemo, useState } from "react";
+
+const OrderContext = createContext();
+
+const pricePerItem = {
+  products: 1000,
+  options: 500,
+};
+
+function calculateSubtotal(orderType, orderCounts) {
+  let optionCount = 0;
+
+  for (const count of orderCounts[orderType].values()) {
+    optionCount += count;
+  }
+
+  return optionCount * pricePerItem[orderType];
+}
+
+export function OrderContextProvider(props) {
+  const [orderCounts, setOrderCounts] = useState({
+    products: new Map(),
+    options: new Map(),
+  });
+
+  const [totals, setTotals] = useState({
+    products: 0,
+    options: 0,
+    total: 0,
+  });
+
+  useEffect(() => {
+    const productsTotal = calculateSubtotal("products", orderCounts);
+    const optionsTotal = calculateSubtotal("options", orderCounts);
+    const total = productsTotal + optionsTotal;
+
+    setTotals({
+      products: productsTotal,
+      options: optionsTotal,
+      total,
+    });
+  }, [orderCounts]);
+
+  const value = useMemo(() => {
+    function updateItemCount(itemName, newItemCount, orderType) {
+      const newOrderCounts = { ...orderCounts };
+      const orderCountsMap = orderCounts[orderType];
+
+      orderCountsMap.set(itemName, parseInt(newOrderCounts));
+
+      setOrderCounts(newOrderCounts);
+    }
+
+    return [{ ...orderCounts, totals }, updateItemCount];
+  }, [orderCounts, totals]);
+
+  return <OrderContext.Provider value={value} {...props} />;
+}
